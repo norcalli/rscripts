e1d86efd3dba7e1bc9cfe178bac18687df0451d6
Ashkan Kiani <ashkan@Aidins-iMac.local>
2015-01-23 15:08:48 -0800

    Made Init return error instead of logging. Oops.

--- rscripts.go
+++ rscripts.go
@@ -2,24 +2,23 @@ package rscripts
 
 import (
 	"github.com/garyburd/redigo/redis"
-	"log"
 )
 
 var incrAddSha string
 
-func AddScript(client redis.Conn) {
+func AddScripts(client redis.Conn) error {
 	var err error
 	incrAddSha, err = redis.String(client.Do("SCRIPT", "LOAD", "local x=redis.call('incr', KEYS[1]);redis.call('sadd', KEYS[2], x);return x"))
 	if err != nil {
-		log.Fatalln(err)
+		return err
 	}
-	log.Println(incrAddSha)
+	return nil
 }
 
 func IncrementAndAdd(client redis.Conn, idKey, setKey string) (int64, error) {
 	return redis.Int64(client.Do("EVALSHA", incrAddSha, 2, idKey, setKey))
 }
 
-func Init(client redis.Conn) {
-	AddScript(client)
+func Init(client redis.Conn) error {
+	return AddScripts(client)
 }
